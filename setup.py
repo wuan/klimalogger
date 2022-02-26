310bd08bb7e95f9b5d36dded9775dc17ff472ee6
Andreas Würl <8093294-wuerla@users.noreply.gitlab.com>
2022-02-26 19:16:30 +0100

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import glob
 
 setup(
     name='klimalogger',
-    version='0.4.2',
+    version='0.4.3',
     packages=find_packages(exclude=('tests')),
     scripts=glob.glob('scripts/*'),
     url='https://github.com/wuan/klimalogger',
