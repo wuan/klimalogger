38c08b90a2eb36513bf39fa0ff668a997eb56cb9
Andreas Würl <8093294-wuerla@users.noreply.gitlab.com>
2022-02-27 12:34:20 +0100

    add baseline values for SGP30 measurements

--- klimalogger/sensor/sgp30_sensor.py
+++ klimalogger/sensor/sgp30_sensor.py
@@ -18,10 +18,14 @@ class Sensor:
 
     @inject
     def __init__(self, config_parser: configparser.ConfigParser):
+        self.baseline_eCO2 = int(config_parser.get('sgp30_sensor', 'baseline_eCO2'))
+        self.baseline_TVOC = int(config_parser.get('sgp30_sensor', 'baseline_TVOC'))
+
         i2c_bus = busio.I2C(board.SCL, board.SDA, frequency=100000)
         self.sensor = adafruit_sgp30.Adafruit_SGP30(i2c_bus)
 
     def measure(self, data_builder):
+        self.sensor.set_iaq_baseline(self.baseline_eCO2, self.baseline_TVOC)
         eCO2, TVOC = self.sensor.iaq_measure()
 
         data_builder.add(self.name, "eCO2", "ppm", float(eCO2))
