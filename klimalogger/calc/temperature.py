f612da4dfbefd4e8c9d0b96ac178cd4c3d794665
Andreas Würl <andi@tryb.de>
2022-12-26 22:41:22 +0100

    update dew point calculation

--- klimalogger/calc/temperature.py
+++ klimalogger/calc/temperature.py
@@ -5,18 +5,22 @@ class TemperatureCalc:
 
     def dew_point(self, temperature: float, relative_humidity: float):
         """
-        Dew point calculation using the Arden Buck equation based calculation, see https://en.wikipedia.org/wiki/Dew_point
-        :param temperature: Temperature in degress Celsius
+        Dew point calculation using the calculation published by Mark G. Lawrence (https://journals.ametsoc.org/view/journals/bams/86/2/bams-86-2-225.xml)
+
+        :param temperature: Temperature in degrees Celsius
         :param relative_humidity: Relavite humidity in percent (range 0..1)
+
         :return: Calculated dew point in degrees Celsius
         """
 
-        a = 6.1121  # hPa
-        b = 18.678
-        c = 257.14  # °C
-        d = 234.5  # °C
+        # Parameters according to Alduchov and Eskridge (https://journals.ametsoc.org/view/journals/apme/35/4/1520-0450_1996_035_0601_imfaos_2_0_co_2.xml)
+        a = 17.625
+        b = 243.04 # °C
 
-        gamma = math.log(relative_humidity / 100.0 *
-                         math.exp((b - temperature / d) * (temperature / (c + temperature))))
+        # Formula according to Lawrence:
+        # Ts = (b × alpha(T,RH)) / (a - alpha(T,RH))
+        # with alpha(T,RH) = ln(RH/100) + aT/(b+T)
 
-        return c * gamma / (b - gamma)
+        alpha = math.log(relative_humidity/100) + a * temperature / (b + temperature)
+
+        return (b * alpha) / (a - alpha)
--- tests/test_calc_temperature.py
+++ tests/test_calc_temperature.py
@@ -11,8 +11,9 @@ class TestCalcTemperature:
         return TemperatureCalc()
 
     @pytest.mark.parametrize("temperature,relative_humidity,expected", [
-        (25, 10, -8.74),
-        (50, 90, 47.27)
+        (20, 80, 16.444),
+        (25, 10, -8.75),
+        (50, 90, 47.9)
     ])
     def test_dew_point(self, uut, temperature, relative_humidity, expected):
         result = uut.dew_point(temperature, relative_humidity)
