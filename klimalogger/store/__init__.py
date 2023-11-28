71b64a59d2225ebd7f4dddbe8404aeb508ae06f1
Andreas Würl <andi@tryb.de>
2023-11-29 00:27:38 +0100

    fix class typo

--- klimalogger/store/__init__.py
+++ klimalogger/store/__init__.py
@@ -9,8 +9,8 @@ log = logging.getLogger(__name__)
 
 def influxdb_store_factory(config: Config):
     if config.store_org is not None:
-        from .influxdb2 import InfluxDbStore
-        return InfluxDbStore(config)
+        from .influxdb2 import InfluxDb2Store
+        return InfluxDb2Store(config)
     else:
         from .influxdb import InfluxDbStore
         return InfluxDbStore(config)
