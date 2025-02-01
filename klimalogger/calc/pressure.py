78357c0d690d305d658da545c25436b2abf9bfd5
Andreas Würl <andi@tryb.de>
2025-02-01 01:30:53 +0100

    update sea-level pressure formula

--- klimalogger/calc/pressure.py
+++ klimalogger/calc/pressure.py
@@ -1,4 +1,5 @@
 class PressureCalc:
 
-    def sea_level_pressure(self, pressure: float, temperature: float, elevation: float) -> float:
-        return pressure * pow(1 - (0.0065 * elevation / (temperature + 0.0065 * elevation + 273.15)), -5.255)
+    def sea_level_pressure(self, pressure: float, temperature_celsius: float, elevation: float) -> float:
+        temperature = temperature_celsius + 273.15
+        return pressure * pow(temperature / (temperature + 0.0065 * elevation), -5.255)
